de25d570aefa1e2bed9b8a33bdd8e200e8227e7b
Joseph Hunkeler <jhunkeler@gmail.com>
2014-10-23 00:12:27 -0400

    Refactor project, add HTCondor bindings, add batch processing

--- htc_utils/CLI/wrap.py
+++ htc_utils/CLI/wrap.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 #
-# This file is part of htcondor_utils.
+# This file is part of htc_utils.
 #
-# htcondor_utils is free software: you can redistribute it and/or modify
+# htc_utils is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# htcondor_utils is distributed in the hope that it will be useful,
+# htc_utils is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with htcondor_utils.  If not, see <http://www.gnu.org/licenses/>.
+# along with htc_utils.  If not, see <http://www.gnu.org/licenses/>.
 
 import argparse
 import os
--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
-#!/usr/bin/env python
 from os import unlink
-from os.path import splitext
+from os.path import splitext, basename, relpath, join
 from shutil import copy2
 from setuptools import setup, find_packages
 
-scripts_original = ['condor_split.py', 'condor_wrap.py']
+CLI = relpath('htc_utils/CLI')
+scripts_original = [ join(CLI, 'split.py'), 
+                        join(CLI, 'wrap.py'), 
+                        join(CLI, 'batch.py') ]
 scripts_renamed = [ splitext(x)[0] for x in scripts_original ]
+scripts_renamed = [ 'condor_' + basename(x) for x in scripts_renamed ]
 
 for script, script_renamed in zip(scripts_original, scripts_renamed):
     copy2(script, script_renamed)
 
 setup(
-    name="htcondor_utils",
+    name="htc_utils",
     version="0.1",
     packages=find_packages(),
     scripts=scripts_renamed,
@@ -25,5 +28,5 @@ setup(
 )
 
 for script in scripts_renamed:
-	unlink(script)
+    unlink(script)
 
